ec32b97b8946eb0563ba2b7954d851a13b8e58f1
NaGoo <nagucsengg@gmail.com>
2020-05-12 20:00:43 +0530

    updated

--- apifile.js
+++ apifile.js
@@ -13,11 +13,48 @@ var connInfo = {
 var connection =mysql.createConnection(connInfo);
 app.get('/Candidates', (req, res) =>
 {
-    let sql='select*from Candidates';
+    let sql= ' select*from Candidates ' ;
 
     if(req.query.id)
     {
-        sql=sql+'where id '+ req.query.id;
+        sql=sql+'where id = '+ req.query.id;
+    }
+    if(req.query.candidate_name)
+    {
+       sql='select * from candidates where candidate_name =?';
+       connection.query(sql,[req.query.candidate_name],(err,data) =>{
+           if(err)
+           {
+               res.send(err);
+           }
+           res.send(data);
+       });
+    }
+    if(req.query.candidate_mail)
+    {
+       sql='select * from candidates where candidate_mail =?';
+       connection.query(sql,[req.query.candidate_mail],(err,data) =>{
+           if(err)
+           {
+               res.send(err);
+           }
+           res.send(data);
+       });
+    }
+    if(req.query.candidate_gender)
+    {
+       sql='select * from candidates where candidate_gender =?';
+       connection.query(sql,[req.query.candidate_gender],(err,data) =>{
+           if(err)
+           {
+               res.send(err);
+           }
+           res.send(data);
+       });
+    }
+    if(req.query.candidate_age)
+    {
+        sql=sql+'where candidate_age = '+ req.query.candidate_age;
     }
     connection.query(sql, function(err, data, fields){
         if(err)
@@ -37,12 +74,37 @@ app.get('/Candidates', (req, res) =>
 });
 app.get('/Employee', (req, res) =>
 {
-    let sql='select*from Employee';
+    let sql=' select*from Employee ';
 
     if(req.query.id)
     {
         sql=sql+'where id '+ req.query.id;
     }
+    if(req.query.emp_name)
+    {
+       sql='select * from employees where emp_name =?';
+       connection.query(sql,[req.query.emp_name],(err,data) =>{
+           if(err)
+           {
+               res.send(err);
+           }
+           res.send(data);
+       });
+    }
+    
+    if(req.query.emp_city)
+    {
+       sql='select * from employees where emp_city =?';
+       connection.query(sql,[req.query.emp_city],(err,data) =>{
+           if(err)
+           {
+               res.send(err);
+           }
+           res.send(data);
+       });
+    }
+    
+    
     connection.query(sql, function(err, data, fields){
         if(err)
         {
@@ -61,11 +123,26 @@ app.get('/Employee', (req, res) =>
 });
 app.get('/Training', (req, res) =>
 {
-    let sql='select*from Training';
+    let sql=' select*from Training ';
 
     if(req.query.id)
     {
-        sql=sql+'where id '+ req.query.id;
+        sql=sql+'where id = '+ req.query.id;
+    }
+    if(req.query.syllabus)
+    {
+       sql='select * from employees where syllabus =?';
+       connection.query(sql,[req.query.syllabus],(err,data) =>{
+           if(err)
+           {
+               res.send(err);
+           }
+           res.send(data);
+       });
+    }
+    if(req.query.no_days)
+    {
+        sql=sql+'where no_days = '+ req.query.no_days;
     }
     connection.query(sql, function(err, data, fields){
         if(err)
